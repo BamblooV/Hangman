5d3702d9b6cd31837dabe1419ce282faeb76c12f
Egor Gavrilenko <bambloo769@gmail.com>
2024-10-25 16:19:30 +0300

    refactor: refactor View

--- view/IView.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hangman.view
-{
-    internal interface IView
-    {
-        public void PrintFrame(int frameNumber, int health, string word, List<char> guessedLetters);
-
-        public void PrintPrompt();
-    }
-}
